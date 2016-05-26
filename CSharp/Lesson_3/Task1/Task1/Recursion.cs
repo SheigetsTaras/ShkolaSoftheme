0196f657c27198268f4ad1888d9b04b431ebb58b
SheigetsTaras <targan@ukr.net>
2016-05-26 17:53:01 +0300

    Start create

---
+++ CSharp/Lesson_3/Task1/Task1/Recursion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Recursion
+    {
+        public void StartPoint(int day, int mounth, int year)
+        {
+            if(day == null)
+        }
+
+        private void EnterDate()
+        {
+            int day = 0;
+            int mounth = 0;
+            int year = 0;
+            DateTime date = new DateTime();
+            date.AddDays(day);
+            date.AddMonths(mounth);
+            date.AddYears(year);
+
+
+            if (date.Day ==)
+            Console.Write("Enter day birth: ");
+            day = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter mounth birth: ");
+            mounth = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter year birth: ");
+            year = int.Parse(Console.ReadLine());
+        }
+
+        private void RecursionCalculate()
+        {
+
+        }
+    }
+}
