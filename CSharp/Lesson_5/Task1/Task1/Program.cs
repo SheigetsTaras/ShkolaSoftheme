5bffd42c94f9774c8e38bd37f81f94277f393582
SheigetsTaras <targan@ukr.net>
2016-05-29 21:20:50 +0300

    Create project Lesson_5

---
+++ CSharp/Lesson_5/Task1/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size: ");
+            int size = int.Parse(Console.ReadLine());
+            for(int i = 0; i< size; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j <= size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+
+            int center = size / 2;
+            for (int i = 0; i <= size; i++)
+            {
+                for (int j = 0; j <= size; j++)
+                {
+                    if (i <= center)
+                    {
+                        if (j >= center - i && j <= center + i)
+                            Console.Write("* ");
+                        else
+                            Console.Write("  ");
+                    }
+                    else
+                    {
+                        if (j >= center + i - size + 1 && j <= center - i + size - 1)
+                            Console.Write("* ");
+                        else
+                            Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
