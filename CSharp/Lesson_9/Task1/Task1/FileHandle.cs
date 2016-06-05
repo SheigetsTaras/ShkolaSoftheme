d05856c2a7a1f60abc7b833d7690753d2ae2d9d2
SheigetsTaras <targan@ukr.net>
2016-06-06 00:02:10 +0300

    Create project Lesson 9

---
+++ CSharp/Lesson_9/Task1/Task1/FileHandle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+
+namespace Task1
+{
+
+
+    [Flags]
+    enum FileAccessEnum : short
+    {
+        Read = 1,
+        Write = 2
+    }
+    struct FileHandler
+    {
+        int FileSize;
+        string FileName;
+        string FilePath;
+        FileAccessEnum Access;
+
+        public FileHandler(string FileName, string FilePath, FileAccessEnum Access)
+        {
+            this.FilePath = FilePath;
+            this.FileName = FileName;
+            this.FileSize = 0;
+            this.Access = Access;
+        }
+
+        public void Read(string filePuth)
+        {
+            using (FileStream fileStream = new FileStream(filePuth, FileMode.OpenOrCreate, (FileAccess)FileAccessEnum.Read))
+            {
+                Console.WriteLine("File open for read");
+            }
+
+        }
+
+        public void Write(string filePuth)
+        {
+            using (FileStream fileStream = new FileStream(filePuth, FileMode.OpenOrCreate, (FileAccess)FileAccessEnum.Write))
+            {
+                Console.WriteLine("File open for write");
+            }
+
+        }
+
+        public void ReadWrite(string filePuth)
+        {
+            using (FileStream fileStream = new FileStream(filePuth, FileMode.OpenOrCreate, (FileAccess)FileAccessEnum.Read | (FileAccess)FileAccessEnum.Write))
+            {
+                Console.WriteLine("File open for read or write");
+            }
+
+        }
+
+
+    }
+}
---
+++ CSharp/Lesson_9/Task1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+            string[] fileName = filePath.Split('\\');
+
+            FileHandler fileHandler = new FileHandler(fileName[fileName.Length - 1], filePath, FileAccessEnum.Read | FileAccessEnum.Write);
+
+            fileHandler.Read(filePath);
+            fileHandler.Write(filePath);
+            fileHandler.ReadWrite(filePath);
+        }
+
+    }
+}
